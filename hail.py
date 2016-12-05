543168f0c814abd542881aedd7fd788d754b0a46
zzeleznick <zzeleznick@gmail.com>
2016-12-05 14:40:26 -0800

    tarjan exp

--- hail.py
+++ hail.py
@@ -10,12 +10,10 @@ from validate_input import validate_file
 from _io_utils import parse_run_args, make_weighted_graph, supress_stdout, save_scores, save_path
 from _shared import reverse, find_cover, repeat_find_cover, verify_path
 
-def get_sample_size(graph):
-    edges = graph.edgelist
-    vertices = graph.vertices
-    sample_size = min(len(vertices), 20)
-    print(len(vertices), len(edges), sample_size)
-    return sample_size
+# External Modules
+from tarjan import tarjan
+from tarjan.tc import tc
+import numpy as np
 
 def find_sets(graph):
     edges = [ pair for (pair, w) in graph.edgelist ]
@@ -23,62 +21,72 @@ def find_sets(graph):
     if not edges:
         return [ random.choice(vertices) ]
     paths = OD()
-    sample_size = get_sample_size(graph)
-    # print(edges)
+    t_input = {v: graph[v].edges.keys() for v in graph.vertices}
+    # print(t_input)
+    scc_index = random.choice(vertices)
+    max_scc = 0
+    '''
+    closure = tarjan(t_input)
+    for idx,lv in enumerate(closure):
+        if len(lv) > max_scc:
+            max_scc = len(lv)
+            scc_index = idx
+        print(idx, len(lv), lv)
+    '''
+    closure = tc(t_input)
+    for idx, path in closure.iteritems():
+        if len(path) > max_scc:
+            max_scc = len(path)
+            scc_index = idx
+        # print(idx, len(path), path)
+
     def calc_score(vertices):
         return len(vertices) * sum(graph[v].value for v in vertices)
-    starter_verts = [vertices[k] for k in random.sample(xrange(len(vertices)), sample_size) ]
-    PREV = [(calc_score([v]), OD([(v, True)]) ) for v in starter_verts ]
-    s,p = random.choice(PREV)
-    best_score = s
-    best_path = p.keys()
-    print("Intialized best path to %s" % best_path)
-    for i in range(1, len(graph.vertices)):
-        print("Iteration %s | Size: %s" % (i, len(PREV)))
-        if not PREV:
-            break
-        elif len(PREV) > 500:
-            selection = PREV[:]
-            random.shuffle(selection)
-            PREV = selection[:500]
-            print("Iteration %s | Size: %s" % (i, len(PREV)))
-        starter_verts = [vertices[k] for k in random.sample(xrange(len(vertices)), sample_size) ]
-        CUR = []
-        for score, path in PREV:
-            next_path = deepcopy(path)
-            last, val = next_path.popitem()
-            next_path[last] = val
-            for v in starter_verts:
-                if graph[last].get(v) and v not in next_path:
-                    alt_path = deepcopy(next_path)
-                    alt_path[v] = True
-                    next_score = calc_score(alt_path.keys())
-                    CUR.append((next_score, alt_path))
-                    if next_score >= best_score:
-                        update_prob = min(.99, math.log(float(next_score+1)/(best_score+1)))
-                        if update_prob > random.random():
-                            best_score = next_score
-                            best_path = alt_path.keys()
-        PREV = CUR
-    # exit()
-    for i, path in paths.iteritems():
-        print(i, path)
+
+    print(scc_index, max_scc)
+
+    # best_path = closure[scc_index]
+    best_path = [scc_index]
+
+    best_score = calc_score(best_path)
+
     print(best_score, best_path)
     verify_path(graph, best_path)
-    if 0.99 > random.random():
-        best_path = dfs_extend(graph, best_path)
-    if 0.99 > random.random():
-        best_path = prefix_extend(graph, best_path)
+    paths = []
+    ctr = 0
+    for i in range(1000):
+        path = dfs_extend(graph, best_path, closure)
+        paths.append((calc_score(path), path))
+        if len(paths) < 10:
+            ctr += 1
+        if i < 500 and ctr > 200:
+            break
+        elif i < 100 and ctr > 50:
+            break
+        elif i < 50 and ctr > 25:
+            break
+        elif i < 10 and ctr > 6:
+            break
+    sorted_paths = sorted(paths, reverse=True)
+    print(sorted_paths[0])
+    best_path = sorted_paths[0][1]
     return best_path
 
-def dfs_extend(graph, path):
+def dfs_extend(graph, path, closure):
     mod_path = OD(zip(path, [True]*len(path)))
     last, val = mod_path.popitem()
     mod_path[last] = True
     edges = graph[last].edges
     choices = [e for e in edges if e not in mod_path ]
     while choices:
-        last = random.choice(choices)
+        weights = np.array([np.e ** float(len(closure[e])) for e in choices]) + \
+                  np.array([np.e ** float(len(closure[e])) for e in choices])
+        total = np.sum(weights)
+        if total <= 0:
+            break
+        weights /= total
+        last = np.random.choice(choices, p=weights)
+        # last = random.choice(choices)
         mod_path[last] = True
         edges = graph[last].edges
         choices = [e for e in edges if e not in mod_path ]
@@ -125,15 +133,16 @@ def find_and_save_path(filename, seed):
 def gen_scores(filename, r=20):
     fpath = "final_inputs/%s" % filename
     graph = make_weighted_graph(*validate_file(fpath))
-    # scores = repeat_find_cover(graph, find_sets, r)
-    random.seed(12)
-    find_cover(graph, find_sets)
+    scores = repeat_find_cover(graph, find_sets, r)
+    # random.seed(10)
+    # np.random.seed(10)
+    # find_cover(graph, find_sets)
     outname = "output/%s_hail.txt" % filename.split(".in")[0]
     # save_scores(scores, outname)
 
 def main():
     args = parse_run_args()
-    filename = args.filename if args.filename else "1.in"
+    filename = args.filename if args.filename else "0082.in"
     reps = args.reps
     if filename == "0070.in":
         print("Activating Hack")
